8c4a33c6a93c864d73b94a15e511c1aa3d217683
NotFound441 <beatrice.pss777@gmail.com>
2021-04-28 13:57:14 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '78e1fecabacbf9c1bd7e320e0361f6c148cf7797fefe7a0446f51cd9f270bc98c2c16130dd9f473184dc59d24c1431b578e4784e6eb8c0dbecdfc58258adcc37'
+
